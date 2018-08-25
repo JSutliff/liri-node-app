f293450a7308dcb4d4c71ad4002f35cc26af75b7
Jason Sutliff <jbopsut@gmail.com>
2018-08-25 15:11:20 -0700

    variables assigned to data and checkCommand function implemented for valid command verification

--- liri.js
+++ liri.js
@@ -5,5 +5,48 @@ var keys = require('./keys.js');
 // var spotify = new Spotify(keys.spotify);
 
 var userInput = process.argv.splice(2);
-
 console.log(userInput);
+
+var liriCommand = userInput[0];
+var searchParams = userInput.splice(1).map(elem => elem.toLocaleLowerCase()).join('+');
+
+var concert = 'concert-this';
+var spotify = 'spotify-this-song';
+var movie = 'movie-this';
+var doWhat = 'do-what-it-says';
+
+//call appropriate APIs
+function callBandsTown() {
+  
+}
+
+
+
+//check liriCommand against list of possible commands
+function checkCommand(intendedCommand) {
+  var validCommand;
+  if (intendedCommand === concert) {
+    validCommand = true;
+  } else if (intendedCommand === spotify) {
+    validCommand = true;
+  } else if (intendedCommand === movie) {
+    validCommand = true;
+  } else if (intendedCommand === doWhat) {
+    validCommand = true;
+  } else {
+    validCommand = false;
+    console.log('Invalid Command');
+  }
+  
+  return validCommand;
+}
+
+checkCommand(liriCommand);
+
+  //if valid command perfom associated task
+  // function getTask(input) {
+  //   if (checkCommand(input) && input === concert) {
+
+  //   }
+  // }
+
