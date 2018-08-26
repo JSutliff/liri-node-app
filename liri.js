c1eff13250e04b9fbe228df70b5933837e1c2c52
Jason Sutliff <jbopsut@gmail.com>
2018-08-25 19:03:45 -0700

    omdb request implemented

--- liri.js
+++ liri.js
@@ -46,6 +46,23 @@ function callSpotify() {
   });
 }
 
+function callOmdb() {
+  request("http://www.omdbapi.com/?apikey=trilogy&t=" + searchParams, function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+    var movieObj = JSON.parse(body);
+    console.log('The title of the movie is: ' + movieObj.Title);
+    console.log('This movie was released in: ' + movieObj.Year);
+    console.log('The IMDB Rating of the movie is: ' + movieObj.imdbRating);
+    console.log('The Rotten Tomatoes Rating of the movie is: ' + movieObj.Ratings[1]);
+    console.log('This movie was produced in: ' + movieObj.Country);
+    console.log('This movie is in: ' + movieObj.Language);
+    console.log('The plot of the movie is: ' + movieObj.Plot);
+    console.log('Actors: ' + movieObj.Actors);
+  }
+});
+}
 
 //check liriCommand against list of possible commands
 function checkCommand(intendedCommand) {
@@ -55,8 +72,10 @@ function checkCommand(intendedCommand) {
     callBandsTown();
   } else if (intendedCommand === spotify) {
     validCommand = true;
+    callSpotify();
   } else if (intendedCommand === movie) {
     validCommand = true;
+    callOmdb();
   } else if (intendedCommand === doWhat) {
     validCommand = true;
   } else {
@@ -68,7 +87,7 @@ function checkCommand(intendedCommand) {
 }
 
 checkCommand(liriCommand);
-callSpotify();
+
 
   //if valid command perfom associated task
   // function getTask(input) {
